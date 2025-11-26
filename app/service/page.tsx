4241d6d90cf7f5576ce7eedc4a487529761ff32d
Mbra Oliver <96344566+Mbra-Oliver@users.noreply.github.com>
2025-11-26 11:14:10 +0000

    Corrige

--- app/service/page.tsx
+++ app/service/page.tsx
@@ -1,20 +1,5 @@
-
-import Link from "next/link";
 function page() {
-  return (
-    <div className="p-4">
-      <div>
-        <h1>Avec HTML CLASSIQUE</h1>
-        <a href="/service">Page service</a> <br />
-        <a href="/blog">Page blog</a>
-        <br />
-        <a href="/news">Page news</a> <br />
-        Avec Next JS (LINK)
-        <br />
-        <Link href={"/service"}>Page service</Link>
-      </div>
-    </div>
-  );
+  return <div>page</div>;
 }
 
 export default page;
