1f91757c197f97bf6a8e5231d6c3bf415c47332e
MariamSanogo <sanogole30@gmail.com>
2025-11-26 11:25:53 +0000

    nav

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,16 +1,13 @@
 import Link from "next/link";
 function page() {
   return (
-    <div className="p-4">
-      <div>
-        <h1>Avec HTML CLASSIQUE</h1>
-        <a href="/service">Page service</a> <br />
-        <a href="/blog">Page blog</a>
-        <br />
-        <a href="/news">Page news</a> <br />
-        Avec Next JS (LINK)
-        <br />
-        <Link href={"/service"}>Page service</Link>
+    <div>
+      <div className="p-6">
+        <p>contact</p>
+        <Link href={"/about"}>about</Link> <br />
+        <Link href={"/blog"}>blog</Link> <br />
+        <Link href={"/news"}>news</Link> <br />
+        <Link href={"/contact"}>contact</Link> <br />
       </div>
     </div>
   );
--- app/service/page.tsx
+++ app/service/page.tsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
 function page() {
-  return <div>page</div>;
+  return (
+    <div>
+      <div className="p-6">
+        <p>mes pages de navigation</p>
+        <Link href={"/about"}>about</Link> <br />
+        <Link href={"/blog"}>blog</Link> <br />
+        <Link href={"/news"}>news</Link> <br />
+        <Link href={"/contact"}>contact</Link> <br />
+      </div>
+    </div>
+  );
+  
+
+
 }
 
 export default page;
